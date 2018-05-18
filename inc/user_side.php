9b62abcf7119d73a51d1f8d341c284b16e085d9b
webcartel <webcartel@yandex.ru>
2018-05-18 16:20:52 +0300

    save

--- inc/user_side.php
+++ inc/user_side.php
@@ -6,5 +6,15 @@ function chats_user_init() {
 }
 
 function chats_app_tag() {
-	echo '<div id="chats"></div>';
+	echo '<div id="chats-user" class="chats-user"><ul><li v-for="item in messageList">{{ item.name }}</li></ul></div>';
 }
+
+function chats_user_css_js($value='')
+{
+	wp_enqueue_script('chats-user-vue', 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js', array(), null, 'in_footer');
+	wp_enqueue_script('chats-user-app', CHATS_PLUGIN_DIR_URL . 'js/app-user.js', array('chats-user-vue'), null, 'in_footer');
+	wp_enqueue_style( 'chats-user-css', CHATS_PLUGIN_DIR_URL . 'css/app-user.css' );
+	// wp_enqueue_style( 'chatsusercss', CHATS_PLUGIN_DIR_URL . 'css/main.css' );
+	// wp_enqueue_script('chatsuserjs', CHATS_PLUGIN_DIR_URL . 'js/main.js', array('jquery'), null, 'in_footer');
+}
+add_action( 'wp_enqueue_scripts', 'chats_user_css_js' );
---
+++ js/app-user.js
@@ -0,0 +1,24 @@
+var chatsUser = new Vue({
+	el: '#chats-user',
+
+	data: {
+		message: 'Content',
+		messageList: [
+			{ name: 'test name' },
+			{ name: 'test name' },
+			{ name: 'test name' },
+			{ name: 'test name' },
+			{ name: 'test name' },
+			{ name: 'test name' },
+			{ name: 'test name' },
+		],
+	},
+
+	mounted: {
+		
+	},
+
+	methods: {
+		
+	}
+})
